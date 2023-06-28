2c6e1d9b3f3172238b29e37b677c9f0ed274c414
Felipeballarino <Felipeballarino97@gmail.com>
2023-06-28 14:27:44 -0300

    dialog image

--- src/components/client/vehiculos/id/carousel.jsx
+++ src/components/client/vehiculos/id/carousel.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 // Import Swiper React components
 import { Swiper, SwiperSlide } from "swiper/react";
 
@@ -10,7 +10,24 @@ import "swiper/css/pagination";
 // import required modules
 import { Navigation, Pagination, Mousewheel, Keyboard } from "swiper";
 
+import {
+  Dialog,
+  DialogHeader,
+  DialogBody,
+  DialogFooter,
+  Avatar,
+  Typography,
+  Card,
+} from "@material-tailwind/react";
+
 const Carousel = ({ arr_imgs }) => {
+  const [open, setOpen] = useState(false);
+  const [imgSelect, setImgSelect] = useState(arr_imgs[0]);
+  const handleOpen = (img) => {
+    setImgSelect(img);
+    setOpen(() => !open);
+  };
+
   return (
     <>
       <Swiper
@@ -26,10 +43,48 @@ const Carousel = ({ arr_imgs }) => {
         {arr_imgs &&
           arr_imgs.map((img, i) => (
             <SwiperSlide key={i}>
-              <img src={img} alt="foto" className="w-full " />
+              <Card onClick={() => handleOpen(img)}>
+                <img src={img} alt="foto" className="w-full " />
+              </Card>
             </SwiperSlide>
           ))}
       </Swiper>
+      <Dialog size="lg" open={open} handler={handleOpen}>
+        <DialogHeader className="justify-between">
+          <div className="flex items-center gap-3">
+            <Avatar
+              size="sm"
+              variant="square"
+              alt="candice wu"
+              src="/vector.png"
+            />
+            <div className="-mt-px flex flex-col">
+              <Typography
+                variant="small"
+                color="blue-gray"
+                className="font-medium"
+              >
+                Multicars
+              </Typography>
+              <Typography
+                variant="small"
+                color="gray"
+                className="text-xs font-normal"
+              >
+                @multicars.vm
+              </Typography>
+            </div>
+          </div>
+        </DialogHeader>
+        <DialogBody divider={true} className="p-0">
+          <img
+            alt=""
+            className=" w-full object-cover object-center"
+            src={imgSelect}
+          />
+        </DialogBody>
+        <DialogFooter></DialogFooter>
+      </Dialog>
     </>
   );
 };
