1eba41728c1604470d6248974d7513f11839ca1c
Julian <jtelgarecz@gmail.com>
2023-07-04 19:38:53 -0300

    saved

--- src/utils/usos.js
+++ src/utils/usos.js
@@ -97,3 +97,18 @@ class DatosSendMsj {
   nombre = ""; // nombre de quien envía el msj
   telefono = ""; // telefono de quien envía el msj
 }
+
+export class DatosMessagePilot {
+  action = "create"; // * Valor fijo "create".
+  appkey = ""; // *
+  pilot_firstname = ""; // *
+  pilot_cellphone = "";
+  pilot_email = "";
+  pilot_contact_type_id = 1; // * 1: Electrónico, 2: Telefónico , 3: Entrevista
+  pilot_business_type_id = 2; // *  (1) Convencional / 0km | (2) Usados | (3) Plan de Ahorro.
+  pilot_notes = ""; // Comentarios, observaciones
+  pilot_suborigin_id = ""; // * Código que identifica el origen primario del Lead.
+  pilot_car_brand = ""; // 	Marca del vehículo de interés Ej: Ford.
+  pilot_car_modelo = ""; // Modelo del vehículo de interés Ej: Fiesta.
+  pilot_provider_url = ""; // URL del servicio que recolectó el dato.
+}
